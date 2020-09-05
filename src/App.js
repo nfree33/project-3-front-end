895d80d41cefe0240aa1ce72ca5ce6c22336dbae
lakergirl702 <jesshoran87@gmail.com>
2020-09-05 13:18:54 -0700

    fixing

--- src/App.js
+++ src/App.js
@@ -118,22 +118,6 @@ const App = (props) => {
         </Switch>
       </div>
 
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. GEST GROUP EVER. WERE CONNECTED!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
     </div>
   );
 };
