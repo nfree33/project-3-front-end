83fc5609cbe74a2fb2b34986d82bed3fd956e0df
Neil Freeman <freeman.neil92@gmail.com>
2020-09-10 22:05:33 -0400

    please work

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-// const express = require('express');
-// const app = express();
-// const path = require('path')
-// const PORT = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const path = require('path')
+const PORT = process.env.PORT || 3000;
 
-// app.use(express.static('build'))
+app.use(express.static('build'))
 
-// app.get("", (req,res)=>{
-//     res.sendFile(path.join(`${__dirname}/build/index.html`))
-// })
+app.get("", (req,res)=>{
+    res.sendFile(path.join(`${__dirname}/build/index.html`))
+})
 
 
-// app.listen(PORT) 
+app.listen(PORT) 
